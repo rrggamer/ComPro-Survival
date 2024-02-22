247fae8fdbb37bdaf5c9003b485308c1549ad2a5
Your Name <you@example.com>
2024-02-22 13:22:30 +0700

    Player And Enemy

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,79 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Vam Clone");
+   
+    //------------------------------- INITIALIZE --------------------------
+   
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Vam Clone");
 
 
-    sf::RectangleShape birdbox(sf::Vector2f(50, 50));
-    birdbox.setPosition(100, 100);
+    //------------------------------- INITIALIZE --------------------------
 
-    sf::Texture pngbird;
-    pngbird.loadFromFile("bird.png");
 
-    if (!pngbird.loadFromFile("bird.png")) {
-        
-        return EXIT_FAILURE;
+    //------------------------------- LOAD --------------------------------
+    //------------------------------- Enemy -------------------------------
+    
+    sf::Texture enemyTexture;
+    sf::Sprite enemySpirte;
+
+    if (enemyTexture.loadFromFile("Assets/Skelton/Textures/spritesheet.png")) 
+    {
+        std::cout << "Enemy images loaded!" << std:: endl;
+        enemySpirte.setTexture(enemyTexture);
+        enemySpirte.setPosition(sf::Vector2f(400,100));
+
+
+        int XIndex = 0;
+        int YIndex = 2;
+
+        enemySpirte.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+        enemySpirte.setScale(sf::Vector2f(3,3));
+          
     }
+    else
+    {
+        std::cout << "Enemy images Failed to loaded!" << std::endl;
+    }
+    
+    //------------------------------- Enemy -------------------------------
 
-    birdbox.setTexture(&pngbird);
+    //------------------------------- Player -------------------------------
+    sf::Texture playerTexture;
+    sf::Sprite playerSpirte;
 
+   
+    if (playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png"))
+    {
+        std::cout << "Player images loaded!" << std::endl;
+        playerSpirte.setTexture(playerTexture);
+
+        int XIndex = 0;
+        int YIndex = 0;
+
+        playerSpirte.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+        playerSpirte.setScale(sf::Vector2f(2,2)); 
+
+    }
+    else
+    {
+        std::cout << "Player images Failed to loaded!" << std::endl;
+    }
+    
+    //------------------------------- Player -----------------------------
+    //------------------------------- LOAD -------------------------------
 
     float playerSpeed = 200.f; 
-
     sf::Clock clock;
 
+    //Game Loop
     while (window.isOpen()) {
+        
         sf::Time deltaTime = clock.restart();
         float dtSeconds = deltaTime.asSeconds();
+
+
+        //------------------------------- UPDATE -------------------------------
 
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -50,12 +99,23 @@ int main() {
         }
 
         
-        birdbox.move(movement * playerSpeed * dtSeconds);
+        playerSpirte.move(movement * playerSpeed * dtSeconds);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+        {
+            sf::Vector2f position = playerSpirte.getPosition();
+            playerSpirte.setPosition(position + sf::Vector2f(1, 0));
+        }
 
         window.clear();
-        window.draw(birdbox);
+        window.draw(playerSpirte);
+        window.draw(enemySpirte);
         window.display();
+    
+    
     }
+
+        //------------------------------- UPDATE -------------------------------
 
     return 0;
 }

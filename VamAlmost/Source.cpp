bb537aea3800aa445b4f465f3b4adbad757f8b6b
Your Name <you@example.com>
2024-02-27 17:16:05 +0700

    Almost

---
+++ VamAlmost/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Game.h"
+
+
+
+
+int main()
+{
+	//Init srand
+	srand(static_cast<unsigned>(time(NULL)));
+
+
+	//Init Game engine
+	Game game;
+
+	//Game loop
+	while (game.running() && !game.getEndGame())
+	{
+		
+		//Update
+		game.update();
+		//Render
+		game.render();
+	}
+
+
+	//End of application
+	return 0;
+}
